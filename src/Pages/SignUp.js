5a3dca52944cfe61e78f26465cac21c322d6e14f
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-24 16:10:43 +0700

    Modified sign in and sign up

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import LogoIM from '../Assets/logo.png';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import LogoIM from "../Assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
 
+import axios from "axios";
 
 function SignUp() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
   const navigate = useNavigate();
-  const [errors, setErrors] = useState ({});
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -25,57 +26,94 @@ function SignUp() {
   //     return true;
   //   } else {
   //     return false;
-  //   } 
+  //   }
   // }
 
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const newErrors = {};
 
-      if (!name){
-        newErrors.name = 'Please enter your name!';
-      }
-      if (!email){
-        newErrors.email = 'Please enter your email!';
-      } 
-      //   else if (emailAlreadyRegistered(email)) {
-      //   newErrors.email = 'This email has already been registered!';
-      // }
-      if (Object.keys(newErrors).length > 0) {
-        setErrors(newErrors);
-      }
-      else {
-        navigate('/otp');
-      }
+    if (!name) {
+      newErrors.name = "Please enter your name!";
+    }
+    if (!email) {
+      newErrors.email = "Please enter your email!";
+    }
+    //   else if (emailAlreadyRegistered(email)) {
+    //   newErrors.email = 'This email has already been registered!';
+    // }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      await axios
+        .post("http://localhost:5000/v1/im/users/signUp", {
+          username: name,
+          emailAddress: email,
+        })
+        .then((res) => {
+          if (res.data.status === 0) {
+            console.log(res.data);
+            console.log(res.data.msg);
+
+            navigate("/otp");
+          } else if (res.data.status === 1 || res.data.status === 2) {
+            console.log(res.data);
+            console.log(res.data.message);
+          }
+        })
+        .then((err) => {
+          console.log(err, err.message);
+        });
+    }
     // Lakukan validasi form dan proses login
   };
 
   return (
-    <div className='App'>
-    <div className="signup-page-container">
-    <div className="signup-image-container">
-      <img src={LogoIM} alt="" />
-    </div>
-      <div className="signup-form-container">
-        <h2>Create an account</h2>
-        <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Username</label>
-          <input type="text" id="name" value={name} onChange={handleNameChange} />
+    <div className="App">
+      <div className="signup-page-container">
+        <div className="signup-image-container">
+          <img src={LogoIM} alt="" />
         </div>
-          {errors.name && <div className='error-message'>{errors.name}</div>}
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" value={email} onChange={handleEmailChange} />
-          </div>
-          {errors.email && <div className='error-message'>{errors.email}</div>}
-          <button type="submit">Sign Up</button>
-        </form>
-        <h5>Already have an account? <Link to="/sign-in" style={{textDecoration: 'none', fontWeight: 'bold'}}>Sign In</Link></h5>
+        <div className="signup-form-container">
+          <h2>Create an account</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Username</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            {errors.name && <div className="error-message">{errors.name}</div>}
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            {errors.email && (
+              <div className="error-message">{errors.email}</div>
+            )}
+            <button type="submit">Sign Up</button>
+          </form>
+          <h5>
+            Already have an account?{" "}
+            <Link
+              to="/sign-in"
+              style={{ textDecoration: "none", fontWeight: "bold" }}
+            >
+              Sign In
+            </Link>
+          </h5>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
