d4b430ee6d9adc6e522b3b720758e17845e114c9
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-25 11:17:20 +0700

    Modified user data

--- src/Components/data/userdata.js
+++ src/Components/data/userdata.js
@@ -1,53 +1,62 @@
 export const user = [
   {
-    "id": 1,
-    "email": "dkemsley0@deviantart.com",
-    "name": "Darsie",
-    "role" : "User"
-  }, {
-    "id": 2,
-    "email": "wbattisson1@domainmarket.com",
-    "name": "Whitney",
-    "role" : "User"
-  }, {
-    "id": 3,
-    "email": "jblaber2@slideshare.net",
-    "name": "Jereme",
-    "role" : "User"
-  }, {
-    "id": 4,
-    "email": "cdaddow3@reddit.com",
-    "name": "Cleon",
-    "role" : "User"
-  }, {
-    "id": 5,
-    "email": "eedgell4@livejournal.com",
-    "name": "Estel",
-    "role" : "User"
-  }, {
-    "id": 6,
-    "email": "mdelloyd5@imageshack.us",
-    "name": "Miranda",
-    "role" : "User"
-  }, {
-    "id": 7,
-    "email": "eberhard6@ow.ly",
-    "name": "Eberhard",
-    "role" : "User"
-  }, {
-    "id": 8,
-    "email": "kduggleby7@foxnews.com",
-    "name": "Kilian",
-    "role" : "Admin"
-  }, {
-    "id": 9,
-    "email": "jrathbourne8@last.fm",
-    "name": "Janek",
-    "role" : "User"
-  }, {
-    "id": 10,
-    "email": "fdumphries9@marketwatch.com",
-    "name": "Filia",
-    "role" : "User"
-  }
-]
+    id: 1,
+    email: "dkemsley0@deviantart.com",
+    name: "Darsie",
+    role: "User",
+  },
+  {
+    id: 2,
+    email: "wbattisson1@domainmarket.com",
+    name: "Whitney",
+    role: "User",
+  },
+  {
+    id: 3,
+    email: "jblaber2@slideshare.net",
+    name: "Jereme",
+    role: "User",
+  },
+  {
+    id: 4,
+    email: "cdaddow3@reddit.com",
+    name: "Cleon",
+    role: "User",
+  },
+  {
+    id: 5,
+    email: "eedgell4@livejournal.com",
+    name: "Estel",
+    role: "User",
+  },
+  {
+    id: 6,
+    email: "mdelloyd5@imageshack.us",
+    name: "Miranda",
+    role: "User",
+  },
+  {
+    id: 7,
+    email: "eberhard6@ow.ly",
+    name: "Eberhard",
+    role: "User",
+  },
+  {
+    id: 8,
+    email: "kduggleby7@foxnews.com",
+    name: "Kilian",
+    role: "Admin",
+  },
+  {
+    id: 9,
+    email: "jrathbourne8@last.fm",
+    name: "Janek",
+    role: "User",
+  },
+  {
+    id: 10,
+    email: "fdumphries9@marketwatch.com",
+    name: "Filia",
+    role: "User",
+  },
+];
