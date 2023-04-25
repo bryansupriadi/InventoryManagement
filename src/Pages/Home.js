9f3ce6f8c9fdfcb16b3f9e8b78c75334e37128b2
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-25 11:19:05 +0700

    Added get logged in handler

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
-import { useMemo } from 'react';
-import SideBar from '../Components/SideBar';
-import Carousel from '../Components/Carousel';
-import Dropdown from '../Components/Dropdown';
-import {product} from '../Components/data/product';
+import React, { useEffect, useState } from "react";
+import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import SideBar from "../Components/SideBar";
+import Carousel from "../Components/Carousel";
+import Dropdown from "../Components/Dropdown";
+import { product } from "../Components/data/product";
 import SearchBar from "../Components/SearchBar";
-import { COLUMNS } from '../Components/Table';
-import {useTable} from 'react-table';
-import logo from '../Assets/logo.png';
+import { COLUMNS } from "../Components/Table";
+import { useTable } from "react-table";
+import logo from "../Assets/logo.png";
 
-function Home() {
+const Home = () => {
+  const navigate = useNavigate();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const [selected, setSelected] = useState("All time");
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => product, []);
@@ -20,74 +25,83 @@ function Home() {
     const lowercasedValue = value.toLowerCase().trim();
     if (lowercasedValue === "") setFilteredData(data);
     else {
-      const filteredItems = data.filter((item) =>
-        item.name && item.name.toLowerCase().includes(lowercasedValue)
+      const filteredItems = data.filter(
+        (item) => item.name && item.name.toLowerCase().includes(lowercasedValue)
       );
       setFilteredData(filteredItems);
     }
   };
-  
 
   const tableInstance = useTable({
     columns,
     data: filteredData,
-  })
+  });
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = tableInstance
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
 
-  return (
+  const getLoggedIn = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    getLoggedIn();
+  }, [navigate]);
+
+  return isLoggedIn ? (
     <div className="App">
       <div className="home-page-container">
-        <div className='navbar-container'>
-        <img 
-          src= {logo} 
-          width='45'
-          height='45'
-          alt='' />
-        <SideBar/>
-      </div>
-        <Dropdown selected={selected} setSelected={setSelected}/>
-        <Carousel/>
+        <div className="navbar-container">
+          <img src={logo} width="45" height="45" alt="" />
+          <SideBar />
+        </div>
+        <Dropdown selected={selected} setSelected={setSelected} />
+        <Carousel />
         <div className="home-title">
-          <div className='title-container'>
+          <div className="title-container">
             <h1>Inventory Items</h1>
-            <SearchBar handleSearch={handleSearch}/>
+            <SearchBar handleSearch={handleSearch} />
           </div>
         </div>
-        <div className='table-home-page'>
-        <table {...getTableProps()} className='table-container'>
-          <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+        <div className="table-home-page">
+          <table {...getTableProps()} className="table-container">
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                    </th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-                prepareRow(row)
-                return(
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
                   <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  })}
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                      );
+                    })}
                   </tr>
-                )
+                );
               })}
-          </tbody>
-        </table>
-      </div>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  )
-}
+  ) : (
+    navigate("/")
+  );
+};
 
 export default Home;
