e76bf3f443428e6ae26c2d532e914b51685a92e5
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-06-12 15:00:47 +0700

    Updated vendor list page

--- src/Pages/VendorList.js
+++ src/Pages/VendorList.js
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import SideBar from "../Components/SideBar";
 import FloatingActionVendor from "../Components/FloatingAction/FloatingActionVendor";
 
-import data from "../Components/data/vendorData";
+// import data from "../Components/data/vendorData";
 
 import api from "../api";
 
@@ -14,19 +14,21 @@ function VendorList() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [vendor, setVendor] = useState(data);
+  // const [vendor, setVendor] = useState(data);
 
-  // const [vendor, setVendor] = useState([]);
+  const [vendors, setVendors] = useState([]);
   const [keyword, setKeyword] = useState("");
 
   const filterVendor = (event) => {
     const keyword = event.target.value.toLowerCase();
     const filteredVendor =
       keyword !== ""
-        ? vendor.filter((user) => user.name.toLowerCase().indexOf(keyword) > -1)
+        ? vendors.filter(
+            (vendor) => vendor.vendorName.toLowerCase().indexOf(keyword) > -1
+          )
         : filteredVendor;
 
-    setVendor(filteredVendor);
+    setVendors(filteredVendor);
     setKeyword(keyword);
   };
 
@@ -39,23 +41,23 @@ function VendorList() {
     }
   };
 
-  // const getDataVendor = async () => {
-  //   await api
-  //     .get("/v1/im/vendors/", {
-  //       headers: { Authorization: `Bearer ${token}` },
-  //     })
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       setVendor(res.data.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err, err.message);
-  //     });
-  // };
+  const getDataVendor = async () => {
+    await api
+      .get("/v1/im/vendors/", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setVendors(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+      });
+  };
 
   useEffect(() => {
     getLoggedIn();
-    // getDataVendor();
+    getDataVendor();
   }, [navigate]);
 
   return isLoggedIn ? (
@@ -76,13 +78,13 @@ function VendorList() {
         </div>
         <div className="vendor-list-container">
           <ul>
-            {vendor.map((user, index) => (
-              <li key={index}>
+            {vendors.map((vendor, index) => (
+              <li key={vendor._id}>
                 <Link
-                  to={`/vendor-list/${user.name}`}
+                  to={`/vendor-list/${vendor.vendorSlug}`}
                   style={{ textDecoration: "none", color: "white" }}
                 >
-                  {user.name}
+                  {vendor.vendorName}
                 </Link>
               </li>
             ))}
