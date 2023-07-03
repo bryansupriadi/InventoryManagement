021f0cf62265377273e8be49cc77aa46382d397b
Bryan <bryansupriadi717@gmail.com>
2023-07-03 14:12:18 +0700

    carousel + tabel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,48 +1,68 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import product from './data/product';
 
-class AutoCarousel extends React.Component {
-  render() {
-    const totalProduct = product.length;
-    const totalQty = product.reduce((total, item) => total + item.Qty, 0);
-    const totalPrice = product.reduce((total, item) => total + parseInt(item['Total Price'].slice(1)), 0);
-    const outOfStock = product.filter(item => item.Qty <= 5).length;
+const AutoCarousel = ({ products }) => {
+  const nonZeroQtyProducts = useMemo(() => {
+    return products.filter((item) => item['Sub Data'].length > 0);
+  }, [products]);
 
-    const settings = {
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      autoplaySpeed: 3500,
-      slidesToShow: 1,
-      arrows: false,
-    };
-    
-    return (
-      <div className='carousel-wrapper'>
-        <Slider {...settings} className='carousel-container'>
-          <div className='carousel-slide-1'>
-            <h3>Total Product</h3>
-            <p>{totalProduct}</p>
-          </div>
-          <div className='carousel-slide-2'>
-            <h3>Total Quantity</h3>
-            <p>{totalQty}</p>
-          </div>
-          <div className='carousel-slide-3'>
-            <h3>Total Price</h3>
-            <p>${totalPrice}</p>
-          </div>
-          <div className='carousel-slide-4'>
-            <h3>Out of Stock</h3>
-            <p>{outOfStock}</p>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
-}
+  const totalProduct = useMemo(() => nonZeroQtyProducts.length, [nonZeroQtyProducts]);
+  const outOfStock = useMemo(() => {
+    return nonZeroQtyProducts.reduce((count, item) => {
+      if (item['Sub Data'].length < 5) {
+        return count + 1;
+      } else {
+        return count;
+      }
+    }, 0);
+  }, [nonZeroQtyProducts]);
+
+  const totalQty = useMemo(() => {
+    return nonZeroQtyProducts.reduce((total, item) => total + item['Sub Data'].length, 0);
+  }, [nonZeroQtyProducts]);
+
+  const totalPrice = useMemo(() => {
+    return nonZeroQtyProducts.reduce((total, item) => {
+      return (
+        total +
+        item['Sub Data'].reduce((subTotal, subItem) => subTotal + subItem.Price, 0)
+      );
+    }, 0);
+  }, [nonZeroQtyProducts]);
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    slidesToShow: 1,
+    arrows: false,
+  };
+
+  return (
+    <div className='carousel-wrapper'>
+      <Slider {...settings} className='carousel-container'>
+        <div className='carousel-slide-1'>
+          <h3>Total Product</h3>
+          <p>{totalProduct}</p>
+        </div>
+        <div className='carousel-slide-2'>
+          <h3>Total Quantity</h3>
+          <p>{totalQty}</p>
+        </div>
+        <div className='carousel-slide-3'>
+          <h3>Total Price</h3>
+          <p>${totalPrice}</p>
+        </div>
+        <div className='carousel-slide-4'>
+          <h3>Out of Stock</h3>
+          <p>{outOfStock}</p>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default AutoCarousel;
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,22 +1,3 @@
-export const COLUMNS = [
-        {
-            Header: 'Name',
-            accessor: 'Name',
-        },
-        {
-            Header: 'Qty',
-            accessor: 'Qty',
-        },
-        {
-            Header: 'Price',
-            accessor: 'Total Price',
-        },
-        {
-            Header: 'Group',
-            accessor: 'Group',
-        },
-];
-
 export const TABLE = [
     {
         Header: 'No',
