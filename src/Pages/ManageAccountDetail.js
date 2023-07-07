5aa5bcee779dd15d6e99bf3d5bbdf0509ccb400c
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-07 14:52:21 +0700

    Updated manage account detail page

--- src/Pages/ManageAccountDetail.js
+++ src/Pages/ManageAccountDetail.js
@@ -1,72 +1,140 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import logo from '../Assets/logo.png';
-import { user } from '../Components/data/userdata';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import logo from "../Assets/logo.png";
+
+// import { user } from "../Components/data/userdata";
+
+import api from "../api";
 
 function ManageAccountDetail() {
-    const {id: urlId} = useParams();
-    const [filteredUser, setFilteredUser] = useState([]);
-    const [showPopupDelete, setShowPopupDelete] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    useEffect(() => {
-      const foundUser = user.find((userData) => userData.id === parseInt(urlId));
+  const token = localStorage.getItem("token");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [filteredUser, setFilteredUser] = useState([]);
+  const [showPopupDelete, setShowPopupDelete] = useState(false);
+
+  const togglePopupDelete = () => {
+    setShowPopupDelete(!showPopupDelete);
+  };
+
+  const handleDelete = (id) => {
+    console.log("Delete user with ID:", id);
+
+    setShowPopupDelete(!showPopupDelete);
+  };
+
+  const handleSignOut = async () => {
+    await api
+      .get("/v1/im/users/signOut")
+      .then((res) => {
+        console.log(res.data);
+        console.log(res.data.msg);
+
+        localStorage.removeItem("token");
+
+        navigate("/sign-in");
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+      });
+  };
+
+  const getLoggedIn = () => {
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      navigate("/sign-in");
+    }
+  };
+
+  const getUserData = async () => {
+    await api
+      .get(`/v1/im/users/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setFilteredUser(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+      });
+  };
+
+  useEffect(() => {
+    getLoggedIn();
+    getUserData();
+
+    const foundUser = filteredUser.find(
+      (userData) => userData._id === parseInt(id)
+    );
     setFilteredUser(foundUser);
-    }, [urlId]);
+  }, [id, navigate]);
 
-    const togglePopupDelete = () => {
-      setShowPopupDelete(!showPopupDelete);
-    }
-
-    const handleDelete = () => {
-      console.log('Delete product with ID:', urlId);
-      togglePopupDelete();
-    }
-
-      
+  const PopupDelete = () => {
     return (
-    <div className='App'>
-    <div className='manage-account-detail-container'>
-    <div className='navbar-super-admin-container'>
-    <img src={logo} width="40" height="40" alt="" />
-    <h1>
-      <Link
-        to="/"
-        // onClick={handleSignOut}
-        style={{ textDecoration: "none", color: "#ff3333" }}
-      >
-        Sign Out
-      </Link>
-    </h1>
-    </div>
-    <div className='user-detail'>
-    <ul>
-    {filteredUser ? (
-        <li key={filteredUser.id}>
-        <p>ID: {filteredUser.id}</p>
-        <p>Name: {filteredUser.name}</p>
-        <p>Email: {filteredUser.email}</p>
-        <p>Role: {filteredUser.role}</p>
-        </li>
-    ) : (
-      <p>user not found</p>
-    )}
-    </ul>
-    </div>
-    <button type="submit" className="btn-user-detail" onClick={togglePopupDelete}>
-    Delete
-    </button>
-    </div>
-    {showPopupDelete && (
-      <div className='popup-delete-container'>
-      <h3>Are you sure you want to delete this user?</h3>
-      <div className="button-container">
-        <button className="cancel-button" onClick={togglePopupDelete}>Cancel</button>
-        <button className="delete-button" onClick={handleDelete}>Delete</button>
+      <div className="popup-delete-container">
+        <h3>Are you sure you want to delete this user?</h3>
+        <div className="button-container">
+          <button className="cancel-button" onClick={togglePopupDelete}>
+            Cancel
+          </button>
+          <button
+            className="delete-button"
+            onClick={handleDelete(filteredUser._id)}
+          >
+            Delete
+          </button>
+        </div>
       </div>
+    );
+  };
+
+  return isLoggedIn ? (
+    <div className="App">
+      <div className="manage-account-detail-container">
+        <div className="navbar-super-admin-container">
+          <img src={logo} width="40" height="40" alt="" />
+          <h1>
+            <Link
+              // to="/"
+              onClick={handleSignOut}
+              style={{ textDecoration: "none", color: "#ff3333" }}
+            >
+              Sign Out
+            </Link>
+          </h1>
+        </div>
+        <div className="user-detail">
+          <ul>
+            {filteredUser ? (
+              <li key={filteredUser._id}>
+                <p>ID: {filteredUser.userId}</p>
+                <p>Name: {filteredUser.username}</p>
+                <p>Email: {filteredUser.emailAddress}</p>
+                <p>Role: {filteredUser.role}</p>
+              </li>
+            ) : (
+              <p>user not found</p>
+            )}
+          </ul>
+        </div>
+        <button
+          type="submit"
+          className="btn-user-detail"
+          onClick={togglePopupDelete}
+        >
+          Delete
+        </button>
       </div>
-    )}
+      {showPopupDelete && <PopupDelete />}
     </div>
-  )
+  ) : (
+    navigate("/sign-in")
+  );
 }
 
-export default ManageAccountDetail
+export default ManageAccountDetail;
