eb31c0b22d3cf07ece4f09499eaea1bb48b5862c
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-07 14:52:47 +0700

    Updated product brand page

--- src/Pages/ProductBrands.js
+++ src/Pages/ProductBrands.js
@@ -1,22 +1,30 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import SideBar from "../Components/SideBar";
-import product from "../Components/data/product";
+// import product from "../Components/data/product";
+
+import api from "../api";
 
 function ProductBrands() {
-  const { group, item, name } = useParams();
+  const { categorySlug, subCategorySlug, productSlug } = useParams();
+
+  const token = localStorage.getItem("token");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [product, setProduct] = useState([]);
 
   // Filter produk yang sesuai dengan sub kategori yang dipilih
   const filteredProducts = product.filter(
-    (item) => item["Sub Category"] === name
+    (item) => item.subCategory.subCategoryName === subCategorySlug
   );
 
   // Ambil merek produk, jumlah, dan harga yang ada pada produk yang sudah difilter
   const productsInfo = filteredProducts.map((item) => ({
-    brandName: item["Brand Name"],
-    quantity: item["Qty"],
-    price: item["Total Price"],
+    brandName: item.brandName,
+    quantity: item.quantity,
+    eachPrice: item.eachPrice,
   }));
 
   // Tampilkan merek produk, jumlah, dan harga dalam list
@@ -29,12 +37,7 @@ function ProductBrands() {
         >
           <Link
             to={{
-              pathname: `/${group}-category/${item}/${name}/${info.brandName}`,
-              state: {
-                group: group,
-                subCategory: name,
-                brandName: info.brandName,
-              },
+              pathname: `/${productsInfo.group}-category/${categorySlug}/${subCategorySlug}/${productSlug}`,
             }}
             style={{ textDecoration: "none", color: "white" }}
             className="list-brands"
@@ -44,7 +47,7 @@ function ProductBrands() {
               {info.quantity && <h6>Quantity: {info.quantity}</h6>}
             </div>
             <div className="price-info">
-              <h6>{info.price}</h6>
+              <h6>{info.eachPrice}</h6>
             </div>
           </Link>
         </li>
@@ -56,11 +59,11 @@ function ProductBrands() {
     <div className="App">
       <div className="product-brand-page-container">
         <div className="navbar-container">
-          <h1>{item}</h1>
+          <h1>{product.categoryName}</h1>
           <SideBar />
         </div>
         <div className="sub-title-product">
-          <h3>{name}</h3>
+          <h3>{product.brandName}</h3>
         </div>
         <div className="content-list">{content}</div>
       </div>
