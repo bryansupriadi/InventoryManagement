50e88c1b2338dbe5bd5d1449126562d07fddf5b0
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-07 14:54:21 +0700

    Updated index

--- src/index.js
+++ src/index.js
@@ -17,25 +17,32 @@ import Home from "./Pages/Home";
 
 import PassiveCategory from "./Pages/PassiveCategory";
 import ActiveCategory from "./Pages/ActiveCategory";
+
 import AddProductForm from "./Pages/AddProduct";
+
+import AddProductType from "./Pages/AddProductType";
+
 import VendorList from "./Pages/VendorList";
 import VendorProduct from "./Pages/VendorProduct";
-import ManageAccount from "./Pages/ManageAccount";
+import VendorTable from "./Pages/VendorTable";
 
 import {
   AddSubCategoryActive,
-  AddSubCategoryPassive,
+  // AddSubCategoryPassive,
 } from "./Pages/AddSubCategory";
+
 import ProductBrands from "./Pages/ProductBrands";
-import Scanner from "./Pages/Scanner";
-import Report from "./Pages/Report";
 import ProductBrandsDetail from "./Pages/ProductBrandsDetail";
-import AddProductType from "./Pages/AddProductType";
-import VendorTable from "./Pages/VendorTable";
+
 import ProductSeeAll from "./Pages/ProductSeeAll";
 import ProductDetail from "./Pages/ProductDetail";
 import EditProduct from "./Pages/EditProduct";
+
+import ManageAccount from "./Pages/ManageAccount";
 import ManageAccountDetail from "./Pages/ManageAccountDetail";
+
+import Scanner from "./Pages/Scanner";
+import Report from "./Pages/Report";
 
 const router = createBrowserRouter([
   {
@@ -75,7 +82,7 @@ const router = createBrowserRouter([
     element: <PassiveCategory />,
   },
   {
-    path: "/:group-category/:item",
+    path: "/:groupSlug-category/:subCategorySlug",
     element: <ProductSeeAll />,
   },
   {
@@ -87,7 +94,7 @@ const router = createBrowserRouter([
     element: <VendorList />,
   },
   {
-    path: "/vendor-list/:name",
+    path: "/vendor-list/:vendorSlug",
     element: <VendorProduct />,
   },
   {
@@ -102,13 +109,37 @@ const router = createBrowserRouter([
     path: "/add-sub-category/active",
     element: <AddSubCategoryActive />,
   },
+  // {
+  //   path: "/add-sub-category/passive",
+  //   element: <AddSubCategoryPassive />,
+  // },
   {
-    path: "/add-sub-category/passive",
-    element: <AddSubCategoryPassive />,
+    path: "/:groupSlug-category/:categorySlug/:subCategorySlug",
+    element: <ProductBrands />,
   },
   {
-    path: "/:group-category/:item/:name",
-    element: <ProductBrands />,
+    path: "/:groupSlug-category/:item/:subCategory/:brandName",
+    element: <ProductBrandsDetail />,
+  },
+  {
+    path: "/:groupSlug-category/:categorySlug/:subCategorySlug/:productSlug/:id",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/vendor-list/:categorySlug/:subCategorySlug/:productSlug/:id",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/:groupSlug-category/:categorySlug/:subCategorySlug/:productSlug/add-product-type",
+    element: <AddProductType />,
+  },
+  {
+    path: "/vendor-list/:name/:subCategorySlug/:productSlug",
+    element: <VendorTable />,
+  },
+  {
+    path: "/:groupSlug-category/:categorySlug/:subCategorySlug/:productSlug/:id/edit-product",
+    element: <EditProduct />,
   },
   {
     path: "/scanner",
@@ -117,30 +148,6 @@ const router = createBrowserRouter([
   {
     path: "/reports",
     element: <Report />,
-  },
-  {
-    path: "/:group-category/:item/:subCategory/:brandName",
-    element: <ProductBrandsDetail />,
-  },
-  {
-    path: "/:group-category/:item/:subCategory/:brandName/:id",
-    element: <ProductDetail />,
-  },
-  {
-    path: "/:group-category/:item/:subCategory/:brandName/add-product-type",
-    element: <AddProductType />,
-  },
-  {
-    path: "/vendor-list/:name/:subCategory/:brandName",
-    element: <VendorTable />,
-  },
-  {
-    path: "/:group-category/:item/:subCategory/:brandName/:id/edit-product",
-    element: <EditProduct />,
-  },
-  {
-    path: "/vendor-list/:name/:subCategory/:brandName/:id",
-    element: <ProductDetail />,
   },
 ]);
 
