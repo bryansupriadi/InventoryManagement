811638faf8852eafab823196b1235a6ea1944b1f
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-07 14:58:32 +0700

    Updated floating action category

--- src/Components/FloatingAction/FloatingActionCategory.js
+++ src/Components/FloatingAction/FloatingActionCategory.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import api from "../../api";
 
 function FloatingActionButton(props) {
+  const token = localStorage.getItem("token");
+
   const [showOptions, setShowOptions] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
-  const [category, setCategory] = useState('');
-  const [errors, setErrors] = useState({});
   const [showPopupSuccess, setShowPopupSuccess] = useState(false);
-  const [successMessage, setSuccessMessage] = useState(''); 
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const [categoryName, setCategoryName] = useState("");
+
   const type = props.type;
   const url = `/add-sub-category/${type}`;
 
@@ -19,47 +25,60 @@ function FloatingActionButton(props) {
     setShowPopup(!showPopup);
   };
 
-  const handleCategoryInputChange = (event) => {
-    setCategory(event.target.value);
+  const handleCategoryInputChange = (e) => {
+    setCategoryName(e.target.value);
   };
 
   const resetForm = () => {
-    setCategory('');
-  }
+    setCategoryName("");
+  };
 
-  const handleAddCategory = (event) => {
-    event.preventDefault();
+  const handleAddCategory = async (e) => {
+    e.preventDefault();
     const newErrors = {};
 
-    if (category.trim() === '') {
-        newErrors.category = 'Category name is required';
+    if (categoryName.trim() === "") {
+      newErrors.category = "Category name is required";
     }
 
     if (Object.keys(newErrors).length === 0) {
-        // TODO: process the categoryInput value
-        setErrors('');
-        setSuccessMessage('Category successfully added!');
-        setShowPopupSuccess(true);
-        resetForm();
-        setTimeout(() => {
-          setShowPopupSuccess(false);
-        }, 3500);
-        togglePopup();
+      await api
+        .post(
+          "/v1/im/categories/",
+          { categoryName },
+          { headers: { Authorization: `Bearer ${token}` } }
+        )
+        .then((res) => {
+          console.log(res.data);
+
+          // TODO: process the categoryInput value
+          setErrors("");
+          setSuccessMessage("Category successfully added!");
+          setShowPopupSuccess(true);
+          resetForm();
+          setTimeout(() => {
+            setShowPopupSuccess(false);
+          }, 3500);
+          togglePopup();
+        })
+        .catch((err) => {
+          console.log(err, err.message);
+        });
     } else {
       setErrors(newErrors);
-      setSuccessMessage('');
+      setSuccessMessage("");
     }
-};
+  };
 
-const Popup = ({ message }) => {
-  return (
-    <div className="popup-success">
-      <div className="popup-success-content">
-        <div className="popup-success-message">{message}</div>
+  const Popup = ({ message }) => {
+    return (
+      <div className="popup-success">
+        <div className="popup-success-content">
+          <div className="popup-success-message">{message}</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
   return (
     <div className="floating-action-button-container">
@@ -68,37 +87,52 @@ const Popup = ({ message }) => {
       </button>
       {showOptions && (
         <div className="floating-action-button-options">
-          <div className='fab-option-container'>
-          <span onClick={togglePopup}>Add Category</span>
-          <button className='fab-option' onClick={togglePopup}>
-          2
-          </button>
+          <div className="fab-option-container">
+            <span onClick={togglePopup}>Add Category</span>
+            <button className="fab-option" onClick={togglePopup}>
+              2
+            </button>
           </div>
-          <div className='fab-option-container'>
-          <span><Link to={url} style={{textDecoration: 'none', color: '#002938'}}>Add Sub Category</Link></span>
-          <button className='fab-option'>
-          <Link to={url} style={{textDecoration: 'none', color: '#002938'}}>1</Link>
-          </button>
+          <div className="fab-option-container">
+            <span>
+              <Link
+                to={url}
+                style={{ textDecoration: "none", color: "#002938" }}
+              >
+                Add Sub Category
+              </Link>
+            </span>
+            <button className="fab-option">
+              <Link
+                to={url}
+                style={{ textDecoration: "none", color: "#002938" }}
+              >
+                1
+              </Link>
+            </button>
           </div>
-          </div>
-      )}
-      <div className='show-popup'>
-      {showPopup && (
-        <div className="popup-container">
-          <form onSubmit={handleAddCategory}>
-            <h1>Add Category</h1>
-            <input type="text" placeholder="category name.." value={category} onChange={handleCategoryInputChange} />
-            {errors.category && <div className="error-message-vendor">{errors.category}</div>}
-            <button type="submit">Save</button>
-          </form>
         </div>
       )}
+      <div className="show-popup">
+        {showPopup && (
+          <div className="popup-container">
+            <form onSubmit={handleAddCategory}>
+              <h1>Add Category</h1>
+              <input
+                type="text"
+                placeholder="category name.."
+                value={categoryName}
+                onChange={handleCategoryInputChange}
+              />
+              {errors.category && (
+                <div className="error-message-vendor">{errors.category}</div>
+              )}
+              <button type="submit">Save</button>
+            </form>
+          </div>
+        )}
       </div>
-      {showPopupSuccess && (
-        <Popup
-          message={successMessage}
-        />
-      )}  
+      {showPopupSuccess && <Popup message={successMessage} />}
     </div>
   );
 }
