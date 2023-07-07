577f0587828be275c47b57f25400debf4b6940bc
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-07 14:53:26 +0700

    Updated vendor list pagee

--- src/Pages/VendorList.js
+++ src/Pages/VendorList.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import SideBar from "../Components/SideBar";
-import FloatingActionVendor from "../Components/FloatingAction/FloatingActionVendor";
+
+// import FloatingActionVendor from "../Components/FloatingAction/FloatingActionVendor";
 
 // import data from "../Components/data/vendorData";
 
@@ -14,13 +15,72 @@ function VendorList() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // const [vendor, setVendor] = useState(data);
-
   const [vendors, setVendors] = useState([]);
+  const [vendorName, setVendorName] = useState("");
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [showPopupSuccess, setShowPopupSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+
   const [keyword, setKeyword] = useState("");
 
-  const filterVendor = (event) => {
-    const keyword = event.target.value.toLowerCase();
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  const handleVendorInputChange = (e) => {
+    setVendorName(e.target.value);
+  };
+
+  const resetForm = () => {
+    setVendorName("");
+    setErrors({});
+  };
+
+  const handleAddVendor = async (e) => {
+    e.preventDefault();
+    const newErrors = {};
+
+    if (vendorName.trim() === "") {
+      newErrors.vendorName = "Vendor name is required";
+    }
+
+    if (Object.keys(newErrors).length === 0) {
+      // TODO: process the categoryInput value
+
+      await api
+        .post(
+          "/v1/im/vendors/",
+          { vendorName: vendorName },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+
+          setErrors({});
+          setSuccessMessage(res.data.msg);
+          setShowPopupSuccess(true);
+          resetForm();
+          setTimeout(() => {
+            setShowPopupSuccess(false);
+          }, 3500);
+          togglePopup();
+
+          getDataVendor();
+        })
+        .catch((err) => {
+          console.log(err, err.message);
+        });
+    } else {
+      setErrors(newErrors);
+    }
+  };
+
+  const filterVendor = (e) => {
+    const keyword = e.target.value.toLowerCase();
     const filteredVendor =
       keyword !== ""
         ? vendors.filter(
@@ -60,6 +120,16 @@ function VendorList() {
     getDataVendor();
   }, [navigate]);
 
+  const Popup = ({ message }) => {
+    return (
+      <div className="popup-success">
+        <div className="popup-success-content">
+          <div className="popup-success-message">{message}</div>
+        </div>
+      </div>
+    );
+  };
+
   return isLoggedIn ? (
     <div className="App">
       <div className="vendor-list-page-container">
@@ -77,22 +147,54 @@ function VendorList() {
           />
         </div>
         <div className="vendor-list-container">
-          <ul>
-            {vendors.map((vendor, index) => (
-              <li key={vendor._id}>
-                <Link
-                  to={`/vendor-list/${vendor.vendorSlug}`}
-                  style={{ textDecoration: "none", color: "white" }}
-                >
-                  {vendor.vendorName}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {vendors.length > 0 ? (
+            <ul>
+              {vendors.map((vendor) => (
+                <li key={vendor._id}>
+                  <Link
+                    to={`/vendor-list/${vendor.vendorSlug}`}
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    {vendor.vendorName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No vendor found</p>
+          )}
         </div>
       </div>
       <div className="floating-action-button-container">
-        <FloatingActionVendor />
+        <div className="floating-action-vendor-container">
+          <button className="floating-action-button" onClick={togglePopup}>
+            <span>+</span>
+          </button>
+          <div className="show-popup">
+            {showPopup && (
+              <div className="popup-container">
+                <form onSubmit={handleAddVendor}>
+                  <h1>Add Vendor</h1>
+                  <input
+                    type="text"
+                    name="vendorName"
+                    placeholder="vendor name.."
+                    value={vendorName}
+                    onChange={handleVendorInputChange}
+                  />
+                  {errors.vendorName && (
+                    <div className="error-message-vendor">
+                      {errors.vendorName}
+                    </div>
+                  )}
+                  <button type="submit">Save</button>
+                </form>
+              </div>
+            )}
+          </div>
+          {showPopupSuccess && <Popup message={successMessage} />}
+        </div>
+        ;
       </div>
     </div>
   ) : (
