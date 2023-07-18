ca4eb643c5f71bdbe46add8d58c78ceb568eb4ef
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-18 10:29:24 +0700

    Updated product brand page

--- src/Pages/ProductBrands.js
+++ src/Pages/ProductBrands.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import SideBar from "../Components/SideBar";
-// import product from "../Components/data/product";
 
 import api from "../api";
 
 function ProductBrands() {
-  const { categorySlug, subCategorySlug, productSlug } = useParams();
+  const navigate = useNavigate();
+  const { categorySlug, subCategorySlug } = useParams();
 
   const token = localStorage.getItem("token");
 
@@ -17,57 +17,117 @@ function ProductBrands() {
 
   // Filter produk yang sesuai dengan sub kategori yang dipilih
   const filteredProducts = product.filter(
-    (item) => item.subCategory.subCategoryName === subCategorySlug
+    (item) => item.subCategorySlug === subCategorySlug
   );
 
   // Ambil merek produk, jumlah, dan harga yang ada pada produk yang sudah difilter
   const productsInfo = filteredProducts.map((item) => ({
+    _id: item._id,
     brandName: item.brandName,
+    categoryName: item.categoryName,
+    subCategoryName: item.subCategoryName,
+    groupSlug: item.groupSlug,
+    productSlug: item.productSlug,
     quantity: item.quantity,
     eachPrice: item.eachPrice,
   }));
 
-  // Tampilkan merek produk, jumlah, dan harga dalam list
-  const content = (
-    <ul className="list-brands-container">
-      {productsInfo.map((info) => (
-        <li
-          key={`${info.brandName}-${info.price}`}
-          className={info.quantity ? "has-quantity" : "no-quantity"}
-        >
-          <Link
-            to={{
-              pathname: `/${productsInfo.group}-category/${categorySlug}/${subCategorySlug}/${productSlug}`,
-            }}
-            style={{ textDecoration: "none", color: "white" }}
-            className="list-brands"
-          >
-            <div className="brand-info">
-              <h3>{info.brandName}</h3>
-              {info.quantity && <h6>Quantity: {info.quantity}</h6>}
-            </div>
-            <div className="price-info">
-              <h6>{info.eachPrice}</h6>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+  const getLoggedIn = () => {
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      navigate("/sign-in");
+    }
+  };
 
-  return (
+  const getAllProductBrands = async () => {
+    await api
+      .get(`/v1/im/products?subCategorySlug=${subCategorySlug}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+
+        setProduct(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+      });
+  };
+
+  useEffect(() => {
+    document.title = "Inventory Management - Product Brands";
+
+    getLoggedIn();
+    getAllProductBrands();
+  }, [navigate, categorySlug, subCategorySlug]);
+
+  // Tampilkan merek produk, jumlah, dan harga dalam list
+  // const Content = () => {
+  //   return (
+  //     <ul className="list-brands-container">
+  //       {productsInfo.map((info) => (
+  //         <li
+  //           key={`${info.brandName}-${info.eachPrice}`}
+  //           className={info.quantity ? "has-quantity" : "no-quantity"}
+  //         >
+  //           <Link
+  //             to={`/${info.groupSlug}-category/${categorySlug}/${subCategorySlug}/${info.productSlug}`}
+  //             style={{ textDecoration: "none", color: "white" }}
+  //             className="list-brands"
+  //           >
+  //             <div className="brand-info">
+  //               <h3>{info.brandName}</h3>
+  //               {info.quantity && <h6>Quantity: {info.quantity}</h6>}
+  //             </div>
+  //             <div className="price-info">
+  //               <h6>{info.eachPrice}</h6>
+  //             </div>
+  //           </Link>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // };
+
+  return isLoggedIn ? (
     <div className="App">
       <div className="product-brand-page-container">
         <div className="navbar-container">
-          <h1>{product.categoryName}</h1>
+          <h1>{productsInfo.categoryName}</h1>
           <SideBar />
         </div>
         <div className="sub-title-product">
-          <h3>{product.brandName}</h3>
+          <h3>{productsInfo.brandName}</h3>
         </div>
-        <div className="content-list">{content}</div>
+        <div className="content-list">
+          <ul className="list-brands-container">
+            {productsInfo.map((info) => (
+              <li
+                key={`${info.brandName}-${info.eachPrice}`}
+                className={info.quantity ? "has-quantity" : "no-quantity"}
+              >
+                <Link
+                  to={`/${info.groupSlug}-category/${categorySlug}/${subCategorySlug}/${info.productSlug}/${info._id}`}
+                  style={{ textDecoration: "none", color: "white" }}
+                  className="list-brands"
+                >
+                  <div className="brand-info">
+                    <h3>{info.brandName}</h3>
+                    {info.quantity && <h6>Quantity: {info.quantity}</h6>}
+                  </div>
+                  <div className="price-info">
+                    <h6>{info.eachPrice}</h6>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
+  ) : (
+    navigate("/sign-in")
   );
 }
 
