528bfe6f0d1deec76cc880a8e0aab75c03afbf63
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-18 10:27:24 +0700

    Updated add sub category page

--- src/Pages/AddSubCategory.js
+++ src/Pages/AddSubCategory.js
@@ -5,6 +5,8 @@ import SideBar from "../Components/SideBar";
 
 import api from "../api";
 
+// active sub category
+
 export function AddSubCategoryActive() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
@@ -101,6 +103,8 @@ export function AddSubCategoryActive() {
   };
 
   useEffect(() => {
+    document.title = "Inventory Management - Add Sub Category Product";
+
     getLoggedIn();
     getAllCategories();
   }, [navigate]);
@@ -194,150 +198,185 @@ export function AddSubCategoryActive() {
   );
 }
 
-// export function AddSubCategoryPassive() {
-//   const categories = [
-//     {
-//       type: "active",
-//       values: [
-//         { value: "computerdevices", label: "Computer Devices" },
-//         { value: "householdappliances", label: "Household Appliances" },
-//       ],
-//     },
-//     {
-//       type: "passive",
-//       values: [
-//         { value: "furniture", label: "Furniture" },
-//         { value: "officesupplies", label: "Office Supplies" },
-//       ],
-//     },
-//   ];
+export function AddSubCategoryPassive() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
-//   const passiveCategories = categories.find(
-//     (category) => category.type === "passive"
-//   ).values;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-//   const [category, setCategory] = useState("");
-//   const [subCategoryName, setSubCategoryName] = useState("");
-//   const [subCategoryImage, setSubCategoryImage] = useState("");
-//   const [errors, setErrors] = useState({});
-//   const [showPopupSuccess, setShowPopupSuccess] = useState(false);
-//   const [successMessage, setSuccessMessage] = useState("");
+  const [errors, setErrors] = useState({});
+  const [showPopupSuccess, setShowPopupSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
-//   const handleCategoryChange = (event) => {
-//     setCategory(event.target.value);
-//   };
+  const [categories, setCategories] = useState([]);
 
-//   const handleSubCategoryNameChange = (event) => {
-//     setSubCategoryName(event.target.value);
-//   };
+  const [formValues, setFormValues] = useState({
+    categoryName: null,
+    subCategoryName: "",
+  });
 
-//   const handleSubCategoryImageChange = (event) => {
-//     setSubCategoryImage(event.target.value);
-//   };
+  const [subCategoryImage, setSubCategoryImage] = useState(null);
 
-//   const resetForm = () => {
-//     setCategory("");
-//     setSubCategoryName("");
-//     setSubCategoryImage("");
-//     setErrors({});
-//   };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const newErrors = {};
+    setFormValues((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-//     if (!subCategoryName) {
-//       newErrors.subCategoryName = "Please enter the sub category name!";
-//     }
-//     if (!subCategoryImage) {
-//       newErrors.subCategoryImage = "Please insert the photo!";
-//     }
+  const handleSubCategoryImageChange = (e) => {
+    setSubCategoryImage(e.target.files[0]);
+  };
 
-//     if (Object.keys(newErrors).length === 0) {
-//       setErrors({});
-//       setSuccessMessage("Sub Category successfully added!");
-//       setShowPopupSuccess(true);
-//       resetForm();
-//       setTimeout(() => {
-//         setShowPopupSuccess(false);
-//       }, 3500);
-//     } else {
-//       setErrors(newErrors);
-//       setSuccessMessage("");
-//     }
-//   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-//   const Popup = ({ message }) => {
-//     return (
-//       <div className="popup-success">
-//         <div className="popup-success-content">
-//           <div className="popup-success-message">{message}</div>
-//         </div>
-//       </div>
-//     );
-//   };
+    const newErrors = {};
 
-//   return (
-//     <div className="App">
-//       <div className="add-sub-category-page-container">
-//         <div className="navbar-container">
-//           <h1>Add Sub Category</h1>
-//           <SideBar />
-//         </div>
-//         <div className="form-container">
-//           <form onSubmit={handleSubmit}>
-//             <div>
-//               <label className="form-field">
-//                 Category Name
-//                 <select
-//                   value={category}
-//                   onChange={handleCategoryChange}
-//                   className="select-ctgry-form"
-//                 >
-//                   {passiveCategories.map((category) => (
-//                     <option key={category.value} value={category.value}>
-//                       {category.label}
-//                     </option>
-//                   ))}
-//                 </select>
-//               </label>
-//             </div>
-//             <div>
-//               <label className="form-field">
-//                 Sub category Name
-//                 <input
-//                   type="text"
-//                   value={subCategoryName}
-//                   onChange={handleSubCategoryNameChange}
-//                   className="input-form"
-//                 />
-//               </label>
-//               {errors.subCategoryName && (
-//                 <div className="error-message-1">{errors.subCategoryName}</div>
-//               )}
-//             </div>
-//             <div>
-//               <label className="form-field">
-//                 Sub category Image
-//                 <input
-//                   type="file"
-//                   value={subCategoryImage}
-//                   onChange={handleSubCategoryImageChange}
-//                 />
-//               </label>
-//               {errors.subCategoryImage && (
-//                 <div className="error-message-1">{errors.subCategoryImage}</div>
-//               )}
-//             </div>
-//             <div>
-//               <button type="submit" className="btn-ctgry-form">
-//                 Submit
-//               </button>
-//             </div>
-//           </form>
-//           {showPopupSuccess && <Popup message={successMessage} />}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+    if (!formValues.categoryName) {
+      newErrors.categoryName = "Please enter the category first!";
+    }
+
+    if (!formValues.subCategoryName) {
+      newErrors.subCategoryName = "Please enter the sub category first!";
+    }
+
+    if (!subCategoryImage) {
+      newErrors.subCategoryImage = "Please enter the sub category image first!";
+    }
+
+    const formData = new FormData();
+    formData.append("categoryName", formValues.categoryName);
+    formData.append("subCategoryName", formValues.subCategoryName);
+    formData.append("subCategoryImage", subCategoryImage);
+
+    // tambahkan kode untuk melakukan submit form
+    await api
+      .post("/v1/im/subCategories", formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+
+        setShowPopupSuccess(true);
+        setSuccessMessage(res.data.msg);
+
+        navigate("/passive-category");
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+
+        setErrors(newErrors);
+        setSuccessMessage("");
+      });
+  };
+
+  const getLoggedIn = () => {
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      navigate("/sign-in");
+    }
+  };
+
+  const getAllCategories = async () => {
+    await api
+      .get("/v1/im/categories", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+
+        setCategories(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+      });
+  };
+
+  useEffect(() => {
+    document.title = "Inventory Management - Add Sub Category Product";
+
+    getLoggedIn();
+    getAllCategories();
+  }, [navigate]);
+
+  const Popup = ({ message }) => {
+    return (
+      <div className="popup-success">
+        <div className="popup-success-content">
+          <div className="popup-success-message">{message}</div>
+        </div>
+      </div>
+    );
+  };
+
+  console.log(formValues);
+
+  return isLoggedIn ? (
+    <div className="App">
+      <div className="add-sub-category-page-container">
+        <div className="navbar-container">
+          <h1>Add Sub Category</h1>
+          <SideBar />
+        </div>
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label className="form-field">
+                Category Name
+                <select
+                  name="categoryName"
+                  value={formValues.categoryName}
+                  onChange={handleInputChange}
+                  className="select-ctgry-form"
+                >
+                  {categories.map((category) => (
+                    <option key={category._id} value={category.categoryName}>
+                      {category.categoryName}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+            <div>
+              <label className="form-field">
+                Sub category Name
+                <input
+                  type="text"
+                  name="subCategoryName"
+                  value={formValues.subCategoryName}
+                  onChange={handleInputChange}
+                  className="input-form"
+                />
+              </label>
+              {errors.subCategoryName && (
+                <div className="error-message-1">{errors.subCategoryName}</div>
+              )}
+            </div>
+            <div>
+              <label className="form-field">
+                Sub category Image
+                <input
+                  type="file"
+                  name="subCategoryImage"
+                  onChange={handleSubCategoryImageChange}
+                />
+              </label>
+              {errors.subCategoryImage && (
+                <div className="error-message-1">{errors.subCategoryImage}</div>
+              )}
+            </div>
+            <div>
+              <button type="submit" className="btn-ctgry-form">
+                Submit
+              </button>
+            </div>
+          </form>
+          {showPopupSuccess && <Popup message={successMessage} />}
+        </div>
+      </div>
+    </div>
+  ) : (
+    navigate("/sign-in")
+  );
+}
