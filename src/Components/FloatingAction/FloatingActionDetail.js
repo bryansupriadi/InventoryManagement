30d9f387697a79f11ae0f214f988d2570d0f2109
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-18 10:48:15 +0700

    Updated floating action detail

--- src/Components/FloatingAction/FloatingActionDetail.js
+++ src/Components/FloatingAction/FloatingActionDetail.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function FloatingActionDetail({ group, item, subCategory, brandName }) {
+function FloatingActionDetail({ group, category, subCategory, product }) {
   const [showOptions, setShowOptions] = useState(false);
-
 
   const toggleOptions = () => {
     setShowOptions(!showOptions);
   };
-
 
   return (
     <div className="floating-action-vendor-container">
@@ -17,25 +15,19 @@ function FloatingActionDetail({ group, item, subCategory, brandName }) {
       </button>
       {showOptions && (
         <div className="floating-action-button-options">
-          <div className='fab-option-container'>
+          <div className="fab-option-container">
             <span>
               <Link
-                to={{
-                  pathname: `/${group}-category/${item}/${subCategory}/${brandName}/add-product-type`,
-                  state: { group: group, subCategory: subCategory, brandName: brandName }
-                }}
-                style={{ textDecoration: 'none', color: '#002938' }}
+                to={`/${group}-category/${category}/${subCategory}/${product}/add-product-type`}
+                style={{ textDecoration: "none", color: "#002938" }}
               >
                 Add Product Type
               </Link>
             </span>
-            <button className='fab-option'>
+            <button className="fab-option">
               <Link
-                to={{
-                  pathname: `/${group}-category/${item}/${subCategory}/${brandName}/add-product-type`,
-                  state: { group: group, subCategory: subCategory, brandName: brandName }
-                }}
-                style={{ textDecoration: 'none', color: '#002938' }}
+                to={`/${group}-category/${category}/${subCategory}/${product}/add-product-type`}
+                style={{ textDecoration: "none", color: "#002938" }}
               >
                 1
               </Link>
