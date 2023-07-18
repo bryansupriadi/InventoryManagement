32a4ede3106148632fc0f574c37de69b7713ea68
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-18 10:32:33 +0700

    Updated vendor table page

--- src/Pages/VendorTable.js
+++ src/Pages/VendorTable.js
@@ -1,76 +1,81 @@
-import React, { useMemo } from 'react';
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
-import SideBar from '../Components/SideBar';
-import product from '../Components/data/product';
-import { useTable } from 'react-table';
+import React, { useState, useEffect, useMemo } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+import { useTable } from "react-table";
+
+import SideBar from "../Components/SideBar";
 
 function VendorTable() {
-  const { name: urlVendor, subCategory: urlSubCategory, brandName: urlBrandName } = useParams();
+  const { vendorSlug, categorySlug, subCategorySlug, productSlug } =
+    useParams();
+
   const navigate = useNavigate();
-  const location = useLocation();
 
-  const tableData = useMemo(() => {
-    const filteredProducts = product.filter((item) => {
-      const subData = item['Sub Data'] || [];
-      return (
-        item['Sub Category'] === urlSubCategory &&
-        item['Brand Name'] === urlBrandName &&
-        subData.some((subItem) => subItem['Vendor'] === urlVendor)
-      );
-    });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const subData = filteredProducts.length > 0 ? filteredProducts[0]['Sub Data'] : [];
-    const vendorData = subData.filter((subItem) => subItem['Vendor'] === urlVendor);
-
-    return vendorData.map((item) => ({ ...item }));
-  }, [urlSubCategory, urlBrandName, urlVendor]);
-
-    const handleRowClick = (product) => {
-    navigate(`${location.pathname}/${product['Product Id']}`);
-  };
+  const [product, setProduct] = useState([]);
 
   const columns = useMemo(
     () => [
-      { Header: 'Date', accessor: 'Date' },
-      { Header: 'Price', accessor: 'Price' },
-      { Header: 'Type', accessor: 'Type' },
-      { Header: 'Location', accessor: 'Location' },
-      { Header: 'Condition', accessor: 'Condition' },
+      { Header: "Date", accessor: "purchaseDate" },
+      { Header: "Price", accessor: "eachPrice" },
+      { Header: "Type", accessor: "typeProduct" },
+      { Header: "Location", accessor: "location" },
+      { Header: "Condition", accessor: "productCondition" },
     ],
     []
   );
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data: tableData });
+  const tableData = useMemo(() => {
+    const filteredProducts = product.filter((item) => {
+      const subData = item || [];
+      return (
+        item.subCategory === subCategorySlug &&
+        item.brandName === productSlug &&
+        subData.some((subItem) => subItem.vendor === vendorSlug)
+      );
+    });
+
+    const subData = filteredProducts.length > 0 ? filteredProducts : [];
+    const vendorData = subData.filter(
+      (subItem) => subItem.vendor === vendorSlug
+    );
+
+    return vendorData.map((item) => ({ ...item }));
+  }, [subCategorySlug, productSlug, vendorSlug]);
+
+  const handleRowClick = (id) => {
+    navigate(
+      `/vendor-list/${categorySlug}/:${subCategorySlug}/${productSlug}/:${id}`
+    );
+  };
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: tableData });
 
   return (
-    <div className='App'>
-      <div className='vendor-table-page-container'>
-        <div className='navbar-container'>
-          <h1>{urlVendor}</h1>
+    <div className="App">
+      <div className="vendor-table-page-container">
+        <div className="navbar-container">
+          <h1>{vendorSlug}</h1>
           <SideBar />
         </div>
-        <div className='sub-title-product-1'>
-          <h3>{urlSubCategory}</h3>
-          <h3>{urlBrandName}</h3>
+        <div className="sub-title-product-1">
+          <h3>{subCategorySlug}</h3>
+          <h3>{productSlug}</h3>
         </div>
-        <div className='product-detail-table-container'>
-          <table {...getTableProps()} className='product-table'>
+        <div className="product-detail-table-container">
+          <table {...getTableProps()} className="product-table">
             <thead>
               {headerGroups.map((headerGroup) => (
                 <tr {...headerGroup.getHeaderGroupProps()}>
                   {headerGroup.headers.map((column) => (
                     <th
-                      style={{ padding: '10px' }}
-                      className='table-header-cell'
+                      style={{ padding: "10px" }}
+                      className="table-header-cell"
                       {...column.getHeaderProps()}
                     >
-                      {column.render('Header')}
+                      {column.render("Header")}
                     </th>
                   ))}
                 </tr>
@@ -83,19 +88,19 @@ function VendorTable() {
                   <tr
                     {...row.getRowProps()}
                     onClick={() => handleRowClick(row.original)}
-                    style={{ cursor: 'pointer' }}
+                    style={{ cursor: "pointer" }}
                   >
                     {row.cells.map((cell) => (
                       <td
                         style={{
-                          whiteSpace: 'nowrap',
-                          overflow: 'hidden',
-                          textOverflow: 'ellipsis',
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
                         }}
-                        className='table-body-cell'
+                        className="table-body-cell"
                         {...cell.getCellProps()}
                       >
-                        {cell.render('Cell')}
+                        {cell.render("Cell")}
                       </td>
                     ))}
                   </tr>
