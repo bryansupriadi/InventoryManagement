efbfbdd70f3d932497339c726d124b06ca1eab72
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-18 10:32:46 +0700

    Updated app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import './App.css';
-import SignIn from './Pages/SignIn';
-import Home from './Pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ActiveCategory from './Pages/ActiveCategory'
+import React from "react";
+import SignIn from "./Pages/SignIn";
 
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <SignIn/>
+      <SignIn />
     </div>
   );
 }
 
 export default App;
-
