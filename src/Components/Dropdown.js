f4847c58870f089846ad2db6c079111c274ca66a
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-26 17:22:49 +0700

    Updated dropdown

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,43 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { Icon } from '@iconify/react';
+import React, { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
 
-function Dropdown({selected, setSelected}) {
-    const [isActive, setIsActive] = useState(false);
-    const options = ["Last year", "Last 3 years", "Last 5 years", "All time"]
- 
-    const handleOptionClick = (option) => {
-      setSelected(option);
-      setIsActive(false);
-    }
+function Dropdown({ selected, setSelected }) {
+  const [isActive, setIsActive] = useState(false);
+  const options = ["Last year", "Last 3 years", "Last 5 years", "All time"];
 
-    useEffect(() => {
-      setIsActive(false);
-    }, [selected]);
-    
-    return (
-    <div className='dropdown-container'>
-    <div className='dropdown-page'>
-      <div className='dropdown-btn' onClick={(e) => setIsActive(!isActive)}>
-      {selected}
-      <Icon icon="gridicons:dropdown" width="35" height="35" />
-      </div>
-      {isActive && (
-        <div className='dropdown-content'>
-            {options.map((option, index) => (
-                <div 
-                key={index }
-                onClick={() => handleOptionClick(option)}
-                className='dropdown-item'>
-                {option}
-                </div>
-            ))}
+  const handleOptionClick = (option) => {
+    setSelected(option);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    setIsActive(false);
+  }, [selected]);
+
+  return (
+    <div className="dropdown-container">
+      <div className="dropdown-page">
+        <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+          {selected}
+          <Icon icon="gridicons:dropdown" width="35" height="35" />
         </div>
-      )}
+        {isActive && (
+          <div className="dropdown-content">
+            {options.map((option, index) => (
+              <div
+                key={index}
+                onClick={() => handleOptionClick(option)}
+                className="dropdown-item"
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-
-  )
+  );
 }
 
 export default Dropdown;
-
