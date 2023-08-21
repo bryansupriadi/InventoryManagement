b5dd451d846c32e0f7b97b27d31f49e71d05cda3
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-08-21 15:00:09 +0700

    Updated carousel page

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -5,37 +5,72 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = ({ products }) => {
-  const nonZeroQtyProducts = useMemo(() => {
-    return products.filter((item) => item.length > 0);
-  }, [products]);
+  // const nonZeroQtyProducts = useMemo(() => {
+  //   return products.filter((item) => item.length > 0);
+  // }, [products]);
 
-  const totalProduct = useMemo(
-    () => nonZeroQtyProducts.length,
-    [nonZeroQtyProducts]
+  // const nonZeroQtyProducts = products.filter((item) => item.length > 0);
+
+  // const totalProduct = nonZeroQtyProducts.length;
+
+  // const outOfStock = nonZeroQtyProducts.reduce((count, item) => {w
+  //   if (item.length < 5) {
+  //     return count + 1;
+  //   } else {
+  //     return count;
+  //   }
+  // }, 0);
+
+  // const totalQty = nonZeroQtyProducts.reduce(
+  //   (total, item) => total + item.length,
+  //   0
+  // );
+
+  // const totalPrice = nonZeroQtyProducts.reduce((total, item) => {
+  //   return (
+  //     total +
+  //     item.reduce((subTotal, subItem) => subTotal + subItem.eachPrice, 0)
+  //   );
+  // }, 0);
+
+  // const totalProduct = useMemo(
+  //   () => nonZeroQtyProducts.length,
+  //   [nonZeroQtyProducts]
+  // );
+
+  // const outOfStock = useMemo(() => {
+  //   return nonZeroQtyProducts.reduce((count, item) => {
+  //     if (item.length < 5) {
+  //       return count + 1;
+  //     } else {
+  //       return count;
+  //     }
+  //   }, 0);
+  // }, [nonZeroQtyProducts]);
+
+  // const totalQty = useMemo(() => {
+  //   return nonZeroQtyProducts.reduce((total, item) => total + item.length, 0);
+  // }, [nonZeroQtyProducts]);
+
+  // const totalPrice = useMemo(() => {
+  //   return nonZeroQtyProducts.reduce((total, item) => {
+  //     return (
+  //       total +
+  //       item.reduce((subTotal, subItem) => subTotal + subItem.eachPrice, 0)
+  //     );
+  //   }, 0);
+  // }, [nonZeroQtyProducts]);
+
+  const totalProduct = products.length;
+  const totalQty = products.reduce(
+    (total, product) => total + product.length,
+    0
   );
-
-  const outOfStock = useMemo(() => {
-    return nonZeroQtyProducts.reduce((count, item) => {
-      if (item.length < 5) {
-        return count + 1;
-      } else {
-        return count;
-      }
-    }, 0);
-  }, [nonZeroQtyProducts]);
-
-  const totalQty = useMemo(() => {
-    return nonZeroQtyProducts.reduce((total, item) => total + item.length, 0);
-  }, [nonZeroQtyProducts]);
-
-  const totalPrice = useMemo(() => {
-    return nonZeroQtyProducts.reduce((total, item) => {
-      return (
-        total +
-        item.reduce((subTotal, subItem) => subTotal + subItem.eachPrice, 0)
-      );
-    }, 0);
-  }, [nonZeroQtyProducts]);
+  const totalPrice = products.reduce(
+    (total, product) => total + product.length * product.eachPrice,
+    0
+  );
+  const outOfStock = products.filter((product) => product.length < 5).length;
 
   const [isTabletView, setIsTabletView] = useState(false);
 
@@ -99,7 +134,7 @@ const Carousel = ({ products }) => {
         </div>
         <div className="carousel-slide-3">
           <h3>Total Price</h3>
-          <p>${totalPrice}</p>
+          <p>Rp. {totalPrice}</p>
         </div>
         <div className="carousel-slide-4">
           <h3>Out of Stock</h3>
